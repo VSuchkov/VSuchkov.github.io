b6ab891d4ba74ec73f1199e19bf7d68572e23e43
Vladimir Suchkov <ssvovs@mail.ru>
2017-04-05 23:48:55 +0300

    первый коммит

---
+++ sendemail.php
@@ -0,0 +1,16 @@
+<?php
+$name       = @trim(stripslashes($_POST['name'])); 
+$from       = @trim(stripslashes($_POST['email'])); 
+$to   		= 'ssvovsl@mail.ru';//replace with your email
+
+$headers   = array();
+$headers[] = "MIME-Version: 1.0";
+$headers[] = "Content-type: text/plain; charset=iso-8859-1";
+$headers[] = "From: {$name} <{$from}>";
+$headers[] = "Reply-To: <{$from}>";
+$headers[] = "Subject: {$subject}";
+$headers[] = "X-Mailer: PHP/".phpversion();
+
+mail($to, $subject, $message, $headers);
+
+die;
